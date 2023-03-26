9f75da229c2c1eb89b4c09da84e38eb70173d556
Кирилл <kirillshershan@gmail.com>
2023-03-26 10:28:06 +0300

    update repo

--- Lab 2.2/PPOIS2labGUI.py
+++ Lab 2.2/PPOIS2labGUI.py
@@ -1,19 +1,17 @@
 import sqlite3
 import tkinter as tk
-from tkinter import N, S, E, W
-from tkinter import TOP, BOTTOM, LEFT, RIGHT, END, ALL
 from tkinter import *
-my_path = "C:\\Users\\kiril\\OneDrive\\Документы\\GitHub\\PPOIS_Spring\\Lab 2.2\\PPOIS2lab.db"
+my_path = "C:\\Users\\kyrill\\Documents\\GitHub\\PPOIS_Spring\\Lab 2.2\\PPOIS2lab.db"
 con = sqlite3.connect(my_path)
 cursor = con.cursor()
 my_way = tk.Tk()
 val_1 = 20
 val_2 = 10
-f = open("C:\\Users\\kiril\\OneDrive\\Документы\\GitHub\\PPOIS_Spring\\Lab 2.2\\P.TXT","r+")
+f = open("C:\\Users\\kyrill\\Documents\\GitHub\\PPOIS_Spring\\Lab 2.2\\P.TXT","r+")
 
-
+# "C:\\Users\\kiril\\OneDrive\\Документы\\GitHub\\PPOIS_Spring\\Lab 2.2\\P.TXT","r+"
 def Display_Groups(my_w,cur,val):
-    my_w.title("Таблица с опт")
+    my_w.title("Списки групп")
     my_w.geometry("1200x720") 
     r_set=cur.execute('SELECT * from Students4 WHERE GroupNumber = ?',(val,))
     i=0 # row value inside the loop 
@@ -57,7 +55,7 @@ def Display_rows(my_w,cur,val1,val2):
     b2.grid(row=15,column=3)  
 
     b3 = tk.Button(my_w,  text='3', width=10, 
-               command=lambda: Display_rows(my_way,cursor,val_1,val_2))  
+               command=lambda : Display_rows(my_way,cursor,val_1,val_2))  
     b3.grid(row=15,column=5)    
 
     b4 = tk.Button(my_w,  text='4', width=10, 
@@ -72,49 +70,73 @@ def Display_rows(my_w,cur,val1,val2):
     
 
 
-def Find(my_w):
+def Find(my_w,cur):
     my_w.title("Поиск студента")
     my_w.geometry("400x250") 
     def display_selected(choice):
-        choice = variable.get()
+        # choice = variable.get()
+        # # choice = t1.get("1.0",END)
+        # variable.set(choice)
+        f.write(choice + '\n')
+        choice = f.readlines()[-1]
         print(choice)
         
-        return choice    
-    l2 = tk.Label(my_w,  text='Name: ', width=10,anchor="c" )  
-    l2.grid(row=15,column=1) 
+        return choice  
+    l1 = tk.Label(my_w,  text='Name: ', width=10,anchor="c" )  
+    l1.grid(row=15,column=1) 
+
+    t1 = tk.Text(my_w,  height=1, width=10,bg='white') 
+    t1.grid(row=15,column=2) 
+
+
+
+    # l2 = tk.Label(my_w,  text='Name: ', width=10,anchor="c" )  
+    # l2.grid(row=15,column=1) 
     # add one text box
-    students = ['Anton','Anna', 'Slava','Ivan']
-    variable = StringVar()
-    variable.set(students[0])
+    # students = ['Anton','Anna', 'Slava','Ivan']
+    # variable = StringVar(my_w)
     
-    opt1 = OptionMenu(
-        my_w, 
-        variable, 
-        *students,
-        command=display_selected 
-        )
+    
+
+    # # l2 = tk.Label(my_w,  text='Class: ', width=10 )  
+    # # l2.grid(row=4,column=1) 
+    # opt1 = OptionMenu(
+    #     my_w, 
+    #     variable, 
+    #     "Anna",
+    #     "Anton",
+    #     "Slava"
+        
+    #     )
      
-    opt1.grid(row=15,column=2)
+    # opt1.grid(row=15,column=2)
+    ch = t1.get('1.0','end')
+    print(ch)
+    # 2ch  = display_selected(choice)
 
-    ch  = display_selected
-    variable.set(ch) 
-    b1 = tk.Button(my_w,  text='1', width=10, 
-               command=lambda: Display_find(my_way,cursor,ch))  
-    b1.grid(row=11,column=1)    
+    # variable.set(ch) 
 
-    my_w.mainloop()
-
-def Display_find(my_w,cur,my_group):
-    my_w.title("Найденные студенты")
-    my_w.geometry("1200x250") 
-    
-    r_set = cur.execute('SELECT * FROM Students4 WHERE FullName=?',(my_group,))
+    # b1 = tk.Button(my_w,  text='Поиск', width=10, 
+    #            command=lambda : Display_find(my_way))  
+    # b1.grid(row=16,column=1)    
+    r_set = cur.execute("SELECT * FROM Students4 WHERE FullName=?",[t1.get('1.0',END)])
     i=0 # row value inside the loop 
     for student in r_set: 
         for j in range(len(student)):
             e = tk.Label(my_w, width=10, fg='blue',text=student[j],anchor='w') 
             e.grid(row=i, column=j,padx=2) 
         i=i+1
+    my_w.mainloop()
+
+def Display_find(my_w):
+    my_w.title("Найденные студенты")
+    my_w.geometry("1200x250") 
+
+    
+   
+    con.commit()
+    
+    
     
     my_w.mainloop()
 
@@ -138,9 +160,9 @@ def Display_find(my_w,cur,my_group):
 
 # Display_rows(my_way,cursor,val_1,val_2)
 # Display_button(my_way)
-Find(my_way)
+Find(my_way,cursor)
 # Display_Groups(my_way,cursor,121702)
-
+# Display_find(my_way,cursor,"Anton")
  # opt1 = OptionMenu(
     #     my_w, 
     #     variable, 
--- Lab 2.2/tempCodeRunnerFile.py
+++ Lab 2.2/tempCodeRunnerFile.py
@@ -1,1 +1,2 @@
-    ch  = display_selected
+l2 = tk.Label(my_w,  text='Class: ', width=10 )  
+    # l2.grid(row=4,column=1) 
